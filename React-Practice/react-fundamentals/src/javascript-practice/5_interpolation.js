cac3bfdd21839fb148aa7e9f50b01bff0f1650b4
Bartosz Zarzecki <bartoszzarzecki@Bartoszs-MacBook-Pro.local>
2019-04-09 15:40:00 -0400

    remove-react-practice

--- React-Practice/react-fundamentals/src/javascript-practice/5_interpolation.js
+++
@@ -1,6 +0,0 @@
-const person = {
-  username: "Kenn",
-  profession: "Secret Agent"
-};
-
-console.log(`${person.username}, ${person.profession}`, "extraordinaire"); //Kenn, Secret Agent, Extraordinaire
