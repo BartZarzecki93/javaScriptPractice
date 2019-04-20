872b6c5873bb8268b8b5e1f4d210f76df2595b72
Bartosz Zarzecki <bartoszzarzecki@Bartoszs-MacBook-Pro.local>
2019-04-20 14:18:33 -0400

    extensions

--- 0-JavaScript-PreWork/3-JavaScript-Basics/08-arrays.js
+++ 0-JavaScript-PreWork/3-JavaScript-Basics/08-arrays.js
@@ -1,20 +1,25 @@
-var rainbowColors = ['Red', 'Orange', 'Yellow', 'Green', 'Blue', 'Indigo', 'Violet'];
+var rainbowColors = [
+  'Red',
+  'Orange',
+  'Yellow',
+  'Green',
+  'Blue',
+  'Indigo',
+  'Violet'
+];
 var raceWinners = [33, 72, 64];
+console.log('OUTPUT: raceWinners', raceWinners);
 var myFavoriteThings = ['Broccoli', 46074, 'Zombie Cats From Mars'];
 
+//0        1        2
+var countryArray = ['USA', 'Russia', 'China', 'Poland'];
+console.log(countryArray[2]); //what will print here?
+console.log('Country Array:', countryArray[0]); //And here?
 
-                    //0        1        2
-                    var countryArray = ["USA", "Russia", "China","Poland"];
-console.log(countryArray[2]);  //what will print here?
-console.log("Country Array:", countryArray[0]); //And here?
+function winning() {
+  console.log(countryArray[i]);
+}
 
-function winning()
-        {
-            console.log(countryArray[i]);
-        
-        }
-
-        for(i=0; i<=3; i++)
-        {
-            winning();
-        }
+for (i = 0; i <= 3; i++) {
+  winning();
+}
